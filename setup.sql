2b9a202b838977a3a099f077c1d24c8476a2cde7
olivia-hye <realrtfl@gmail.com>
2020-11-23 20:03:54 -0500

    add daily_streak column in profile

--- setup.sql
+++ setup.sql
@@ -9,8 +9,9 @@ CREATE TABLE profile
     discord_id      VARCHAR(32) NOT NULL,
     private         BOOLEAN DEFAULT 0,
     blurb           TEXT(500),
-    bits            BIGINT UNSIGNED DEFAULT 0,
-    bits_bank       BIGINT UNSIGNED DEFAULT 0,
+    bits            INT UNSIGNED DEFAULT 0,
+    bits_bank       INT UNSIGNED DEFAULT 0,
     daily_last      DATE,
+    daily_streak    SMALLINT UNSIGNED DEFAULT 0,
     PRIMARY KEY(discord_id)
 );
