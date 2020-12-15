9892d49bfce381bfcfb466d1e7747eebd0ff210b
olivia-hye <realrtfl@gmail.com>
2020-12-14 19:14:17 -0500

    add ability to view other peoples tags by id or mention

--- src/commands/game/player/ViewTags.ts
+++ src/commands/game/player/ViewTags.ts
@@ -1,8 +1,9 @@
-import { Message } from "eris";
+import { Message, User } from "eris";
 import { BaseCommand } from "../../../structures/command/Command";
 import { GameProfile } from "../../../structures/game/Profile";
 import { ProfileService } from "../../../lib/database/services/game/ProfileService";
 import { MessageEmbed } from "../../../structures/client/RichEmbed";
+import * as ZephyrError from "../../../structures/error/ZephyrError";
 
 export default class ViewTags extends BaseCommand {
   names = ["viewtags", "tags", "vt"];
@@ -10,13 +11,31 @@ export default class ViewTags extends BaseCommand {
   usage = ["$CMD$"];
 
   async exec(msg: Message, profile: GameProfile): Promise<void> {
-    const userTags = await ProfileService.getTags(profile);
+    let target: GameProfile | undefined;
+    let targetUser: User;
+
+    if (msg.mentions[0]) {
+      targetUser = msg.mentions[0];
+    } else if (!isNaN(parseInt(this.options[0]))) {
+      if (this.options[0].length < 17)
+        throw new ZephyrError.InvalidSnowflakeError();
+
+      targetUser = await this.zephyr.fetchUser(this.options[0]);
+    } else {
+      targetUser = msg.author;
+      target = profile;
+    }
+
+    if (!target) target = await ProfileService.getProfile(targetUser.id);
+
+    const userTags = await ProfileService.getTags(target);
 
     const embed = new MessageEmbed()
       .setAuthor(
         `Tag List | ${msg.author.tag}`,
         msg.author.dynamicAvatarURL("png")
       )
+      .setTitle(`${targetUser.tag}'s tags`)
       .setDescription(
         userTags.length === 0
           ? `You have no tags.`
