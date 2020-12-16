75bdd749394c274e0d0de0f58f07c28016cb1c92
olivia-hye <realrtfl@gmail.com>
2020-12-16 13:02:26 -0500

    document card generation stuff

--- src/lib/database/services/game/CardService.ts
+++ src/lib/database/services/game/CardService.ts
@@ -12,6 +12,7 @@ import { GameTag } from "../../../../structures/game/Tag";
 import gm from "gm";
 
 export abstract class CardService {
+  // Used for card image generation
   private static toBufferPromise(state: gm.State): Promise<Buffer> {
     return new Promise((resolve) => {
       try {
@@ -80,9 +81,11 @@ export abstract class CardService {
   ): Promise<GameUserCard> {
     return await CardSet.createNewUserCard(card, profile, zephyr, price, frame);
   }
+
   public static async getUserCardById(id: number): Promise<GameUserCard> {
     return await CardGet.getUserCardById(id);
   }
+
   public static async getUserCardByIdentifier(
     identifier: string
   ): Promise<GameUserCard> {
@@ -90,9 +93,11 @@ export abstract class CardService {
     if (isNaN(id)) throw new ZephyrError.InvalidCardReferenceError();
     return await this.getUserCardById(id);
   }
+
   public static async getLastCard(discordId: string): Promise<GameUserCard> {
     return await CardGet.getLastCard(discordId);
   }
+
   public static async getUserInventory(
     profile: GameProfile,
     tags: GameTag[],
@@ -100,6 +105,7 @@ export abstract class CardService {
   ): Promise<GameUserCard[]> {
     return await CardGet.getUserInventory(profile, options, tags);
   }
+
   public static async getUserInventorySize(
     profile: GameProfile,
     tags: GameTag[],
@@ -107,86 +113,93 @@ export abstract class CardService {
   ): Promise<number> {
     return await CardGet.getUserInventorySize(profile, options, tags);
   }
+
   public static async generateCardImage(
     card: GameUserCard,
     zephyr: Zephyr
   ): Promise<Buffer> {
+    // Need information off the base card to do anything.
     const baseCard = zephyr.getCard(card.baseCardId);
 
+    // Create the card canvas
     const canvas = createCanvas(350, 500);
     const ctx = canvas.getContext("2d");
 
+    // Load the base card image (the subject of the card)
     let img = await loadImage(baseCard.image);
-    const overlay = await loadImage(
-      `./src/assets/groups/${
-        baseCard.group?.toLowerCase().replace("*", "") || "nogroup"
-      }.png`
-    );
+
+    // Load the card's frame, default if the id column is null
     let frame: Image;
     if (!card.frameId || !card.frameUrl) {
       frame = await loadImage(`./src/assets/frames/frame-white.png`);
     } else frame = await loadImage(card.frameUrl);
 
-    /*ctx.save();
-    ctx.shadowBlur = 15;
-    ctx.shadowOffsetX = 0;
-    ctx.shadowOffsetY = 0;
-    ctx.shadowColor = "#18700C";
-    ctx.fill();
-
-    ctx.drawImage(frame, 0, 0, 350, 500);
-    ctx.restore();
-
-    ctx.drawImage(frame, 0, 0, 350, 500);
-    ctx.save();
-    ctx.globalCompositeOperation = "destination-atop";
-    ctx.fillStyle = "#18700C";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.restore();*/
+    // Draw the base image, then the frame on top of that
     ctx.drawImage(img, 0, 0, 350, 500);
     ctx.drawImage(frame, 0, 0, 350, 500);
 
+    // Handle dye mask (this was so annoying to set up I hate Windows)
     if (card.dyeMaskUrl) {
-      const dyeMask = gm(card.dyeMaskUrl).colorspace("RGB");
+      // Default to the classic "Undyed Mask Gray" if the card is undyed.
       let [r, g, b] = [card.dyeR || 255, card.dyeG || 255, card.dyeB || 255];
 
-      const dyeBuffer = await this.toBufferPromise(dyeMask.colorize(r, g, b));
+      // We need to convert the GM State to a buffer, so that
+      // canvas knows what to do with it.
+      const dyeBuffer = await this.toBufferPromise(
+        gm(card.dyeMaskUrl).colorize(r, g, b)
+      );
 
+      // Load the buffer and draw the dye mask on top of the frame.
       const dyeImage = await loadImage(dyeBuffer);
       ctx.drawImage(dyeImage, 0, 0, 350, 500);
     }
 
+    // Draw the group icon
+    const overlay = await loadImage(
+      `./src/assets/groups/${
+        baseCard.group?.toLowerCase().replace("*", "") || "nogroup"
+      }.png`
+    );
     ctx.drawImage(overlay, 0, 0, 350, 500);
 
+    // Draw the serial number
     ctx.font = "20px AlteHaasGroteskBold";
     ctx.fillText(`#${card.serialNumber}`, 50, 421);
+
+    // Draw the name of the subject
     ctx.font = "30px AlteHaasGroteskBold";
     ctx.fillText(`${baseCard.name}`, 50, 445);
 
+    // Send it off!
     const buf = canvas.toBuffer("image/png");
-    const final = Buffer.alloc(buf.length, buf, "base64");
-
-    return final;
+    return Buffer.alloc(buf.length, buf, "base64");
   }
 
   public static async generateCardCollage(
     cards: GameUserCard[],
     zephyr: Zephyr
   ): Promise<Buffer> {
+    // Create the collage canvas
     const canvas = createCanvas(cards.length * 250, 333);
     const ctx = canvas.getContext("2d");
 
+    // Draw the background - this will be a jpeg, so we need a
+    // Discord-colored background to look transparent.
+    // Sorry lightmode users!
     ctx.beginPath();
     ctx.rect(0, 0, cards.length * 250, 333);
     ctx.fillStyle = "#36393E";
     ctx.fill();
 
+    // Generate each card image and draw it on the collage
     ctx.font = "14px AlteHaasGroteskBold";
     for (let [i, card] of cards.entries()) {
       const cardBuffer = await this.generateCardImage(card, zephyr);
       const img = await loadImage(cardBuffer);
       ctx.drawImage(img, i * 250, 0, 250, 333);
     }
+
+    // Send it off!
     const buf = canvas.toBuffer("image/jpeg");
     return Buffer.alloc(buf.length, buf, "base64");
   }
@@ -196,6 +209,7 @@ export abstract class CardService {
     background: string,
     zephyr: Zephyr
   ): Promise<Buffer> {
+    /* WIP */
     const canvas = createCanvas(1400, 1000);
     const ctx = canvas.getContext("2d");
 
@@ -228,11 +242,13 @@ export abstract class CardService {
     const newCard = await CardSet.setCardFrame(card, frameId);
     return await this.updateCardCache(newCard, zephyr);
   }
+
   public static async getRandomFrame(
     includeUnshoppable: boolean = false
   ): Promise<GameFrame> {
     return await CardGet.getRandomFrame(includeUnshoppable);
   }
+
   public static async getFrameById(id: number): Promise<GameFrame> {
     return await CardGet.getFrameById(id);
   }
@@ -259,16 +275,26 @@ export abstract class CardService {
     zephyr: Zephyr
   ): Promise<Buffer> {
     const image = await this.generateCardImage(card, zephyr);
+
+    // fs will throw an error if the directory already exists.
+    // TODO - Change this do detect directory, removing need for error handling.
     try {
       await fs.mkdir(`./cache/cards/${card.baseCardId}`);
     } catch (e) {}
+
+    // Overwrite the cached card image.
+    // The card image file stays in the temp folder while it's building.
+    // Once it's finished, it's moved to the cache folder to avoid
+    // showing users incomplete card images.
     await fs.writeFile(`./cache/cards/temp/${card.id}`, image);
     await fs.rename(
       `./cache/cards/temp/${card.id}`,
       `./cache/cards/${card.baseCardId}/${card.id}`
     );
+
     return await fs.readFile(`./cache/cards/${card.baseCardId}/${card.id}`);
   }
+
   public static async checkCacheForCard(
     card: GameUserCard,
     zephyr: Zephyr
@@ -286,6 +312,7 @@ export abstract class CardService {
   ): Promise<void> {
     return await CardSet.setCardsTag(cards, tagId);
   }
+
   public static async unsetCardsTag(cards: GameUserCard[]): Promise<void> {
     return await CardSet.unsetCardsTag(cards);
   }
@@ -296,6 +323,7 @@ export abstract class CardService {
   ): Promise<number> {
     return await CardGet.getNumberOfTopCollectors(ids, zephyr.user.id);
   }
+
   public static async getTopCollectorsByBaseIds(
     ids: number[],
     zephyr: Zephyr,
@@ -307,6 +335,7 @@ export abstract class CardService {
   public static async getNumberOfTopWishlisted(): Promise<number> {
     return await CardGet.getNumberOfTopWishlisted();
   }
+
   public static async getTopWishlisted(
     page: number = 1
   ): Promise<{ group: string; name: string; count: number }[]> {
