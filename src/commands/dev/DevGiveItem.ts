b9efaaa8f22dd141cca94298a2ed1282805b8c22
olivia-hye <realrtfl@gmail.com>
2020-12-22 11:22:54 -0500

    refactor useitem

---
+++ src/commands/dev/DevGiveItem.ts
@@ -0,0 +1,40 @@
+import { Message } from "eris";
+import { MessageEmbed } from "../../structures/client/RichEmbed";
+import { BaseCommand } from "../../structures/command/Command";
+import { GameProfile } from "../../structures/game/Profile";
+import * as ZephyrError from "../../structures/error/ZephyrError";
+import { items } from "../../assets/items.json";
+import { ProfileService } from "../../lib/database/services/game/ProfileService";
+
+export default class DevUserCard extends BaseCommand {
+  names = ["dgi"];
+  description = `Gives someone an item.`;
+  developerOnly = true;
+
+  async exec(msg: Message, _profile: GameProfile): Promise<void> {
+    const targetUser = msg.mentions[0];
+    if (!targetUser) throw new ZephyrError.InvalidMentionError();
+
+    const targetItem = items.filter(
+      (i) =>
+        i.name.toLowerCase() === this.options.slice(1).join(" ").toLowerCase()
+    )[0];
+    if (!targetItem) throw new ZephyrError.InvalidItemError();
+
+    const targetProfile = await ProfileService.getProfile(targetUser.id);
+
+    await ProfileService.addItems(targetProfile, [
+      { item: targetItem, count: 1 },
+    ]);
+
+    const embed = new MessageEmbed()
+      .setAuthor(
+        `Item Giver | ${msg.author.tag}`,
+        msg.author.dynamicAvatarURL("png")
+      )
+      .setDescription(
+        `Gave **1x** \`${targetItem.name}\` to **${targetUser.tag}**`
+      );
+    await msg.channel.createMessage({ embed });
+  }
+}
--- src/commands/game/items/UseItem.ts
+++ src/commands/game/items/UseItem.ts
@@ -1,4 +1,4 @@
-import { Message } from "eris";
+import { Message, PartialEmoji } from "eris";
 import { BaseCommand } from "../../../structures/command/Command";
 import { GameProfile } from "../../../structures/game/Profile";
 import { ProfileService } from "../../../lib/database/services/game/ProfileService";
@@ -10,6 +10,7 @@ import { MessageEmbed } from "../../../structures/client/RichEmbed";
 import chromajs from "chroma-js";
 import { getNearestColor } from "../../../lib/ZephyrUtils";
 import { createCanvas } from "canvas";
+import { ReactionCollector } from "eris-collector";
 
 export default class UseItem extends BaseCommand {
   names = ["use"];
@@ -25,115 +26,186 @@ export default class UseItem extends BaseCommand {
 
     if (!targetItem) throw new ZephyrError.InvalidItemError();
 
-    const userItem = await ProfileService.getItem(
-      profile,
-      targetItem.id,
-      targetItem.name
-    );
+    switch (targetItem.type) {
+      case "FRAME": {
+        const cardIdentifier = this.options.slice(
+          targetItem.name.split(" ").length
+        )[0];
+        if (!cardIdentifier) throw new ZephyrError.InvalidCardReferenceError();
 
-    if (targetItem.type === "FRAME") {
-      const offset = this.options.slice(targetItem.name?.split(" ").length);
-      const identifier = offset[0];
-      if (!identifier) throw new ZephyrError.InvalidCardReferenceError();
+        if (isNaN(parseInt(cardIdentifier, 36)))
+          throw new ZephyrError.InvalidCardReferenceError();
 
-      const id = parseInt(identifier, 36);
-      if (isNaN(id)) throw new ZephyrError.InvalidCardReferenceError();
+        const card = await CardService.getUserCardByIdentifier(cardIdentifier);
+        if (card.discordId !== msg.author.id)
+          throw new ZephyrError.NotOwnerOfCardError(card);
 
-      const card = await CardService.getUserCardById(id);
-      if (card.discordId !== msg.author.id)
-        throw new ZephyrError.NotOwnerOfCardError(card);
+        const frame = await ShopService.getFrameByName(targetItem.name);
 
-      const frame = await ShopService.getFrameByName(targetItem.name);
-      await CardService.changeCardFrame(card, frame.id, this.zephyr);
-      await ProfileService.removeItems(profile, [
-        { item: targetItem, count: 1 },
-      ]);
-      const embed = new MessageEmbed()
-        .setAuthor(
-          `Use Item | ${msg.author.tag}`,
-          msg.author.dynamicAvatarURL("png")
-        )
-        .setDescription(
-          `${this.zephyr.config.discord.emoji.check} You used \`${
-            targetItem.name
-          }\` on \`${card.id.toString(36)}\`.` +
-            `\n— You now have **${userItem.quantity - 1}x** \`${
-              targetItem.name
-            }\`.`
-        );
-      await msg.channel.createMessage({ embed });
-      return;
-    } else if (targetItem.type === "COUPON") {
-      const match = targetItem.name.match(/\d+/g);
-      if (!match) throw new ZephyrError.InvalidAmountError("bits");
-      let amount = parseInt(match.join(""), 10);
+        card.dyeMaskUrl = frame.dyeMaskUrl;
+        card.frameUrl = frame.frameUrl;
 
-      if (isNaN(amount)) throw new ZephyrError.InvalidAmountError("bits");
-
-      await ProfileService.removeItems(profile, [
-        { item: targetItem, count: 1 },
-      ]);
-      const newProfile = await ProfileService.addBitsToProfile(profile, amount);
-      const embed = new MessageEmbed()
-        .setAuthor(
-          `Use Item | ${msg.author.tag}`,
-          msg.author.dynamicAvatarURL("png")
-        )
-        .setDescription(
-          `${this.zephyr.config.discord.emoji.check} You used \`${
-            targetItem.name
-          }\` and received ${
-            this.zephyr.config.discord.emoji.bits
-          }**${amount.toLocaleString()}**` +
-            `\n— You now have **${newProfile.bits.toLocaleString()}** bits.`
-        );
-      await msg.channel.createMessage({ embed });
-      return;
-    } else if (targetItem.type === "CONSUMABLE") {
-      if (targetItem.id === 20) {
-        await ProfileService.removeItems(profile, [
-          { item: targetItem, count: 1 },
-        ]);
-        const randomColor = chromajs.random();
-        const rgb = randomColor.rgb();
-        const hex = randomColor.hex();
-        const colorName = getNearestColor(hex);
-
-        const newDye = await ProfileService.addDye(profile, {
-          r: rgb[0],
-          g: rgb[1],
-          b: rgb[2],
-        });
-
-        const canvas = createCanvas(100, 100);
-        const ctx = canvas.getContext("2d");
-
-        ctx.fillStyle = hex;
-        ctx.fillRect(0, 0, 100, 100);
-
-        const buffer = canvas.toBuffer("image/jpeg");
-        const buf = Buffer.alloc(buffer.length, buffer, "base64");
+        const preview = await CardService.generateCardImage(card, this.zephyr);
 
         const embed = new MessageEmbed()
           .setAuthor(
-            `Dye Bottle | ${msg.author.tag}`,
+            `Apply Frame | ${msg.author.tag}`,
             msg.author.dynamicAvatarURL("png")
           )
           .setDescription(
-            `You got...` +
-              `\n\`$${newDye.id.toString(36)}\` **${colorName.name}** [**${
-                newDye.charges
-              }**]!`
+            `Really apply **${targetItem.name}** to \`${card.id.toString(
+              36
+            )}\`?`
           )
-          .setThumbnail(`attachment://dye.png`)
-          .setColor(hex);
+          .setImage(`attachment://preview.png`);
 
-        await msg.channel.createMessage(
+        const confirmation = await msg.channel.createMessage(
           { embed },
-          { file: buf, name: "dye.png" }
+          { file: preview, name: "preview.png" }
         );
 
-        return;
+        const filter = (_m: Message, emoji: PartialEmoji, userId: string) =>
+          userId === msg.author.id &&
+          emoji.id === this.zephyr.config.discord.emojiId.check;
+
+        const collector = new ReactionCollector(
+          this.zephyr,
+          confirmation,
+          filter,
+          {
+            time: 30000,
+            max: 1,
+          }
+        );
+
+        collector.on("collect", async () => {
+          const refetchCard = await CardService.getUserCardById(card.id);
+          if (refetchCard.discordId !== msg.author.id) {
+            await confirmation.edit({
+              embed: embed.setFooter(
+                `⚠️ ${refetchCard.id.toString(36)} does not belong to you.`
+              ),
+            });
+            return;
+          }
+
+          await confirmation.delete();
+
+          const newCard = await CardService.changeCardFrame(
+            card,
+            frame.id,
+            this.zephyr
+          );
+
+          await ProfileService.removeItems(profile, [
+            { item: targetItem, count: 1 },
+          ]);
+
+          embed.setDescription(
+            `You applied **${targetItem.name}** to \`${card.id.toString(36)}\`.`
+          );
+          embed.setImage(`attachment://success.png`);
+          await msg.channel.createMessage(
+            { embed },
+            { file: newCard, name: "success.png" }
+          );
+
+          collector.stop();
+          return;
+        });
+
+        collector.on("end", async (_collected: unknown, reason: string) => {
+          if (reason === "time") {
+            await confirmation.edit({
+              embed: embed.setFooter(`🕒 This confirmation has expired.`),
+            });
+          }
+
+          try {
+            await confirmation.removeReactions();
+          } catch {}
+        });
+
+        await confirmation.addReaction(
+          `check:${this.zephyr.config.discord.emojiId.check}`
+        );
+        break;
+      }
+      case "COUPON": {
+        const match = targetItem.name.match(/\d+/g);
+        if (!match) throw new ZephyrError.InvalidAmountError("bits");
+        let amount = parseInt(match.join(""), 10);
+
+        if (isNaN(amount)) throw new ZephyrError.InvalidAmountError("bits");
+
+        await ProfileService.removeItems(profile, [
+          { item: targetItem, count: 1 },
+        ]);
+        const newProfile = await ProfileService.addBitsToProfile(
+          profile,
+          amount
+        );
+        const embed = new MessageEmbed()
+          .setAuthor(
+            `Use Item | ${msg.author.tag}`,
+            msg.author.dynamicAvatarURL("png")
+          )
+          .setDescription(
+            `${this.zephyr.config.discord.emoji.check} You used \`${
+              targetItem.name
+            }\` and received ${
+              this.zephyr.config.discord.emoji.bits
+            }**${amount.toLocaleString()}**` +
+              `\n— You now have **${newProfile.bits.toLocaleString()}** bits.`
+          );
+        await msg.channel.createMessage({ embed });
+        break;
+      }
+      case "CONSUMABLE": {
+        if (targetItem.id === 20) {
+          await ProfileService.removeItems(profile, [
+            { item: targetItem, count: 1 },
+          ]);
+          const randomColor = chromajs.random();
+          const rgb = randomColor.rgb();
+          const hex = randomColor.hex();
+          const colorName = getNearestColor(hex);
+
+          const newDye = await ProfileService.addDye(profile, {
+            r: rgb[0],
+            g: rgb[1],
+            b: rgb[2],
+          });
+
+          const canvas = createCanvas(100, 100);
+          const ctx = canvas.getContext("2d");
+
+          ctx.fillStyle = hex;
+          ctx.fillRect(0, 0, 100, 100);
+
+          const buffer = canvas.toBuffer("image/jpeg");
+          const buf = Buffer.alloc(buffer.length, buffer, "base64");
+
+          const embed = new MessageEmbed()
+            .setAuthor(
+              `Dye Bottle | ${msg.author.tag}`,
+              msg.author.dynamicAvatarURL("png")
+            )
+            .setDescription(
+              `You got...` +
+                `\n\`$${newDye.id.toString(36)}\` **${colorName.name}** [**${
+                  newDye.charges
+                }**]!`
+            )
+            .setThumbnail(`attachment://dye.png`)
+            .setColor(hex);
+
+          await msg.channel.createMessage(
+            { embed },
+            { file: buf, name: "dye.png" }
+          );
+          break;
+        }
       }
     }
   }
