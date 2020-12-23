133361b9bbc0a12d233c6863c33e8e3714657072
olivia-hye <realrtfl@gmail.com>
2020-12-23 01:00:14 -0500

    add invite command

---
+++ src/commands/info/Invite.ts
@@ -0,0 +1,23 @@
+import { Message } from "eris";
+import { MessageEmbed } from "../../structures/client/RichEmbed";
+import { BaseCommand } from "../../structures/command/Command";
+import { GameProfile } from "../../structures/game/Profile";
+
+export default class Patreon extends BaseCommand {
+  names = ["invite", "inv", "support"];
+  description =
+    "Sends a link to the Zephyr Community server, and a link to add the bot.";
+  usage = ["$CMD$"];
+  async exec(msg: Message, _profile: GameProfile): Promise<void> {
+    const embed = new MessageEmbed()
+      .setAuthor(
+        `Invite | ${msg.author.tag}`,
+        msg.author.dynamicAvatarURL("png")
+      )
+      .setDescription(
+        `You can add the bot to your server by [clicking here](https://discord.com/api/oauth2/authorize?client_id=791100707629432863&permissions=388160&scope=bot)!` +
+          `\nYou can also join the Zephyr Community server at https://discord.gg/7PFyqUvKYs!`
+      );
+    await msg.channel.createMessage({ embed });
+  }
+}
