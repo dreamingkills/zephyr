096c1454558ad69060c766f9c0ed3b5d1c42280b
olivia-hye <realrtfl@gmail.com>
2020-12-26 12:40:31 -0500

    create branch

---
+++ src/lib/talents/index.ts
@@ -0,0 +1,7 @@
+import { GameUserCard } from "../../structures/game/UserCard";
+
+function calculateTalents(card: GameUserCard): void {
+  console.log("Hello world")!;
+}
+
+export { calculateTalents };
