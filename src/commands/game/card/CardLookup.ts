396063ff640e27aedf822d9de3817b2ebb8028e1
olivia-hye <realrtfl@gmail.com>
2021-01-30 00:30:15 -0500

    fix lookup bug with falsy selection value

--- src/commands/game/card/CardLookup.ts
+++ src/commands/game/card/CardLookup.ts
@@ -94,7 +94,7 @@ export default class CardLookup extends BaseCommand {
       });
     });
 
-    if (!selection) {
+    if (!selection && selection !== 0) {
       await confirmation.edit({
         embed: confirmationEmbed.setFooter(`🕒 This lookup has timed out.`),
       });
