bda41f126ee09a304c2b8a8b20f895919a4120b7
olivia-hye <realrtfl@gmail.com>
2021-02-10 00:21:58 -0500

    jesus im stupid

--- src/lib/database/sql/meta/anticheat/ACGet.ts
+++ src/lib/database/sql/meta/anticheat/ACGet.ts
@@ -15,7 +15,7 @@ export abstract class ACGet extends DBClass {
 
   public static async getNumberOfVotes(profile: GameProfile): Promise<number> {
     const query = (await DB.query(
-      `SELECT COUNT(*) AS count FROM vote WHERE voter=?;`,
+      `SELECT SUM(1 * (weekend+1)) AS count FROM vote WHERE voter=?;`,
       [profile.discordId]
     )) as { count: number }[];
     return query[0]?.count || 0;
@@ -35,7 +35,7 @@ export abstract class ACGet extends DBClass {
     profile: GameProfile
   ): Promise<number> {
     const query = (await DB.query(
-      `SELECT SUM(1 * (weekend+1)) AS count FROM gift WHERE recipient=?;`,
+      `SELECT COUNT(*) AS count FROM gift WHERE recipient=?;`,
       [profile.discordId]
     )) as { count: number }[];
     return query[0]?.count || 0;
