a2757cb4996be65f08e3f7d531dc7c87351f6385
dreamingkills <realrtfl@gmail.com>
2021-05-31 17:23:59 -0400

    add pack selection

--- src/lib/items/UseStickerPack.ts
+++ src/lib/items/UseStickerPack.ts
@@ -9,6 +9,9 @@ import * as ZephyrError from "../../structures/error/ZephyrError";
 import { Stickers } from "../cosmetics/Stickers";
 import { Zephyr } from "../../structures/client/Zephyr";
 import { makeAndString } from "../utility/text/TextUtils";
+import { MessageCollector } from "eris-collector";
+import { Logger } from "../logger/Logger";
+import { deleteMessage } from "../discord/message/deleteMessage";
 
 export async function useStickerPack(
   msg: Message,
@@ -18,6 +21,82 @@ export async function useStickerPack(
   const targetPack = Stickers.getStickerPackByItemId(packItem.id);
 
   if (!targetPack) throw new ZephyrError.NoPackBoundToItemError();
+
+  if (targetPack.selection) {
+    const stickerStrings = targetPack.stickers.map(
+      (s) => `— \`${targetPack.stickers.indexOf(s) + 1}\` ${s.name}`
+    );
+
+    const confirmationEmbed = new MessageEmbed(
+      `Sticker Pack`,
+      msg.author
+    ).setDescription(
+      `**Please select a sticker by replying with a number!**` +
+        `\n${stickerStrings.join(`\n`)}`
+    );
+
+    const confirmation = await createMessage(msg.channel, confirmationEmbed);
+
+    const selection: number | undefined = await new Promise((res, _req) => {
+      const filter = (m: Message) =>
+        targetPack.stickers[parseInt(m.content) - 1] &&
+        m.author.id === msg.author.id;
+
+      const collector = new MessageCollector(Zephyr, msg.channel, filter, {
+        time: 30000,
+        max: 1,
+      });
+
+      collector.on("error", async (e: Error) => {
+        Logger.error(
+          `[Sticker Pack ID: ${targetPack.id}] Unexpected error opening selectable sticker pack: ${e}`
+        );
+
+        res(-1);
+      });
+
+      collector.on("collect", async (m: Message) => {
+        res(parseInt(m.content) - 1);
+      });
+
+      collector.on("end", async (_c: any, reason: string) => {
+        if (reason === "time") res(undefined);
+      });
+    });
+
+    if (!selection && selection !== 0) {
+      await confirmation.edit({
+        embed: confirmationEmbed.setFooter(
+          `🕒 This selection has timed out. Your sticker pack has not been used.`
+        ),
+      });
+
+      return;
+    }
+
+    const targetSticker = targetPack.stickers[selection];
+
+    const targetStickerItem = getItemById(targetSticker.itemId);
+
+    if (!targetStickerItem) {
+      Logger.error(`[Sticker ID: ${targetSticker.id}] Sticker has no item!`);
+      throw new Error(`An unexpected error occurred.`);
+    }
+
+    await ProfileService.removeItems(profile, [{ item: packItem, count: 1 }]);
+    await ProfileService.addItems(profile, [
+      { item: targetStickerItem, count: 1 },
+    ]);
+
+    await deleteMessage(confirmation);
+
+    const embed = new MessageEmbed(`Sticker Pack`, msg.author).setDescription(
+      `:tada: You opened the **${targetPack.name}** and received **1x ${targetSticker.name}**!`
+    );
+
+    await createMessage(msg.channel, embed);
+    return;
+  }
 
   const pulls: { item: PrefabItem; count: number }[] = [];
 
