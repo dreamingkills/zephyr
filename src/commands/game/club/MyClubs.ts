bbd65df3848150ce44a254048fce99902b66babf
dreamingkills <realrtfl@gmail.com>
2021-05-30 21:15:42 -0400

    add role indicator to MyClubs, member count

--- src/commands/game/club/MyClubs.ts
+++ src/commands/game/club/MyClubs.ts
@@ -1,7 +1,10 @@
 import { Message } from "eris";
 import { BaseCommand } from "../../../structures/command/Command";
 import { GameProfile } from "../../../structures/game/Profile";
-import { getUserClubMembership } from "../../../lib/database/sql/game/club/ClubGetter";
+import {
+  getClubMembers,
+  getUserClubMembership,
+} from "../../../lib/database/sql/game/club/ClubGetter";
 import { MessageEmbed } from "../../../structures/client/RichEmbed";
 import { escapeMarkdown } from "../../../lib/utility/text/TextUtils";
 import { Zephyr } from "../../../structures/client/Zephyr";
@@ -30,16 +33,39 @@ export default class MyClubs extends BaseCommand {
       await this.send(msg.channel, embed);
     }
 
-    embed.setDescription(
-      clubs
-        .map(
-          (club) =>
-            `**${escapeMarkdown(club.name)}** - ${
-              club.ownerId === msg.author.id ? `Owner` : `Member`
-            }`
-        )
-        .join(`\n`)
-    );
+    const owner = [];
+    const moderator = [];
+    const member = [];
+
+    for (let club of clubs) {
+      const isOwner = club.ownerId === msg.author.id;
+
+      const members = await getClubMembers(club);
+
+      const isMod = members.find((m) => m.discordId === msg.author.id)?.isMod;
+
+      if (isOwner) {
+        owner.push(
+          `:crown: **${escapeMarkdown(club.name)}** (${members.length}/${
+            club.memberLimit
+          })`
+        );
+      } else if (isMod) {
+        moderator.push(
+          `:shield: **${escapeMarkdown(club.name)}** (${members.length}/${
+            club.memberLimit
+          })`
+        );
+      } else {
+        member.push(
+          `:bust_in_silhouette: **${escapeMarkdown(club.name)}** (${
+            members.length
+          }/${club.memberLimit})`
+        );
+      }
+    }
+
+    embed.setDescription([...owner, ...moderator, ...member].join(`\n`));
 
     await this.send(msg.channel, embed);
     return;
